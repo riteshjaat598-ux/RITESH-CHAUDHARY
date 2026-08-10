68b4d648d1c500e2ac6a9d5a6bb3abd859306484
riteshjaat598-ux <riteshjaat598@gmail.com>
2026-08-10 23:22:49 +0530

    Add files via upload

---
+++ 1Day.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+
+    printf("A:");
+    scanf("%d",&a);
+     int b;
+     printf("B:");
+     scanf("%d", &b);
+     int c= a+b ;
+     printf("added value is: %d",c);
+    return 0;
+
+}
