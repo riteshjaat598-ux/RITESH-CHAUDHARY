18bc238e30a0f33797edcdd2ff87412b6a1c3648
riteshjaat598-ux <riteshjaat598@gmail.com>
2026-08-13 20:24:28 +0530

    Add files via upload

--- Day4.c
+++ Day4.c
@@ -1,23 +1,20 @@
 #include<stdio.h>
 int main(){
-    int a , b ;
-    printf("A:");
+    int a , b ,sum = 0;
+    printf("ENTER THE NUMBER :");
     scanf("%d",&a);
-    
-    printf("B:");
-    scanf("%d",&b);
 
-    printf("\n BEFORE SWAPPING \n");
-    printf("THE FIRST NUMBER IS:%d\n",a);
-    printf("THE SECOND NUMBER IS:%d\n",b);
 
-    a = a+b;
-    b = a-b;
-    a = a-b;
+    printf("THE FIRST %d NATURAL NUMBERS ARE :",a);
+    for(b=1; b<=a ; b++ ){
+    printf("%d",b);
+    sum += b;
+    }
 
-    printf("\n AFTER SWAPPING \n");
-    printf("THE FIST NUMBER IS:%d\n",a);
-    printf("THE SECOND NUMBER IS:%d\n",b);
+    printf("\n THE SUM OF FIRST %d NATURAL NUMBER IS = %d\n",a,sum);
+
+
+
 
 return 0;
 }
