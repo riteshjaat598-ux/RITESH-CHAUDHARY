d3034ecf630a5eb8cdc46463c915d0f99023fef5
riteshjaat598-ux <riteshjaat598@gmail.com>
2026-08-14 16:30:50 +0530

    Add files via upload

--- Day5.c
+++ Day5.c
@@ -1,29 +1,17 @@
 #include<stdio.h>
-#include<math.h>
+
 int main(){
+    int seconds ;
+    printf("SECONDS :");
+    scanf("%d",&seconds);
 
-    double principal , rate , time ;
-    double simple_interest , compound_interest , total_amount ;
+    int hours , minutes , second ;
 
-    printf("INTEREST CALCLATOR \n");
+    hours = seconds/3600;
+    minutes = (seconds % 3600)/60;
+    seconds = seconds % 60 ;
 
-    printf("ENTER THE PRINCIPAL AMOUNT:");
-    scanf("%lf",&principal);
-
-    printf("ENTER THE ANNUAL RATE:");
-    scanf("%lf",&rate);
-
-    printf("ENTER THE TIME PERIOD:");
-    scanf("%lf",&time);
-
-    simple_interest = (principal*rate*time)/100;
-
-    total_amount = principal * pow((1+rate/100),time);
-    compound_interest = total_amount - principal ;
-
-    printf("\n====== RESULT IS ======\n");
-    printf("SIMPLE INTEREST:%.2lf\n",simple_interest);
-    printf("COMPOUND INTEREST:%.2lf\n",compound_interest);
-
+    printf("%d:%d:%d\n",hours,minutes,seconds);
     return 0;
+
 }
