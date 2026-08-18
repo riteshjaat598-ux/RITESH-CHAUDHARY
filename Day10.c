5a27d00fb3930df00f0c3c5dc45f238d3c23e98c
RITESH CHAUDHARY <riteshjaat598@gmail.com>
2026-08-19 01:12:42 +0530

    Add files via upload

--- Day10.c
+++ Day10.c
@@ -1,26 +1,45 @@
 #include<stdio.h>
-
 int main(){
     int a;
+
     printf("A:");
     scanf("%d",&a);
-    int b;
-    printf("B:");
-    scanf("%d",&b);
-    int c;
-    printf("C:");
-    scanf("%d",&c);
 
-    if( a==b && b==c ){
-         printf("The given triangle is equilateral");
-    } else if (a == b || b == c || c == a) {
-        printf("The given triangle is isosceles");
-    } else {
-        printf("The given triangle is scalene");
-    }
+    switch(a){
+
+        case 1:
+        printf("MONDAY");
+        break;
     
-    return 0;
+        case 2:
+        printf("TUESDAY");
+         break;
+
+        case 3:
+        printf("WEDNESDAY");
+        break;
+
+        case 4:
+        printf("THURSDAY");
+        break;
+
+        case 5:
+        printf("FRIDAY");
+        break;
+
+        case 6:
+        printf("SATURDAY");
+        break;
+
+        case 7:
+        printf("SUNDAY");
+        break;
+
+    }
+    return 0 ;
 }
+
+
 
     
 
