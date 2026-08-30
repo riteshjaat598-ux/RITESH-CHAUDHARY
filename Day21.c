af70956d3f26c7aca8b84761878dbf084e0372b9
RITESH CHAUDHARY <riteshjaat598@gmail.com>
2026-08-30 20:39:53 +0530

    Add files via upload

--- Day21.c
+++ Day21.c
@@ -1,32 +1,29 @@
+
 #include <stdio.h>
 
 int main() {
-    int num, originalNum;
-    int firstDigit, lastDigit;
-    int multiplier = 1;
+    int num, i;
+    int sum = 0;
 
-    printf("Enter any integer: ");
-    if (scanf("%d", &num) != 1) {
-        printf("Invalid input.\n");
-        return 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("%d is not a perfect number.\n", num);
+        return 0;
     }
 
-    originalNum = num;
-
-    lastDigit = num % 10;
-
-    int temp = num;
-    while (temp >= 10) {
-        temp /= 10;
-        multiplier *= 10;
+    for(i = 1; i <= num / 2; i++) {
+        if(num % i == 0) {
+            sum += i;
+        }
     }
-    
-    firstDigit = temp;
 
-    int middlePart = (originalNum % multiplier) / 10;
-    int swappedNum = (lastDigit * multiplier) + (middlePart * 10) + firstDigit;
-    printf("Original number: %d\n", originalNum);
-    printf("Number after swapping first and last digit: %d\n", swappedNum);
+    if(sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
 
     return 0;
 }
