2c51dab5db6134a441b5201de43421722f411a1f
RITESH CHAUDHARY <riteshjaat598@gmail.com>
2026-08-31 21:45:06 +0530

    Add files via upload

--- Day22.c
+++ Day22.c
@@ -1,3 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    double sum = 0.0;
+
+    printf("Enter the number of terms (n): ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Please enter a valid positive integer.\n");
+        return 1;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            sum += 1.0; 
+        } else {
+            double numerator = 2.0 * i - 1.0;
+            double denominator = 2.0 * i;
+            sum += numerator / denominator;
+        }
+    }
+
+    printf("The sum of the series up to %d terms is: %.6lf\n", n, sum);
+
+    return 0;
+}
+
 #include <stdio.h>
 
 int main() {
