a1c037ed77833628ea8a616a22ed0b6cbca0caf7
RITESH CHAUDHARY <riteshjaat598@gmail.com>
2026-09-09 01:36:38 +0530

    Add files via upload

---
+++ Day31II.c
@@ -0,0 +1,33 @@
+#include <stdio.h>  
+
+int main() {
+
+    int n, temp;  
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];  
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+
+        scanf("%d", &arr[i]);  
+    }
+
+    for (int i = 0; i < n / 2; i++) {
+
+        temp = arr[i];
+
+        arr[i] = arr[n - i - 1];
+     
+        arr[n - i - 1] = temp;            
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0; 
+}
