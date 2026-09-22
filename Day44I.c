5b81f95d8baf41bd0b8e51cea171a1f9142e6fdb
RITESH CHAUDHARY <riteshjaat598@gmail.com>
2026-09-22 19:11:33 +0530

    Add files via upload

---
+++ Day44I.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, special = 0;
+    int i = 0;
+
+    printf("Enter any string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        if (str[i] >= '0' && str[i] <= '9') {
+            digits++;
+        } else if (str[i] == ' ') {
+            spaces++;
+        } else if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))) {
+            special++;
+        }
+        i++;
+    }
+
+    printf("Spaces=%d, Digits=%d, Special=%d\n", spaces, digits, special);
+    return 0;
+}
